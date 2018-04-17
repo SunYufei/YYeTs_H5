3ac489334d62231531dbd53f9b62cc207962871d
SunYufei <sunyufei963@gmail.com>
2018-04-17 19:57:31 +0800

    Update 18/4/17

--- app/src/main/java/io/github/sunyufei/yyets/MainActivity.kt
+++ app/src/main/java/io/github/sunyufei/yyets/MainActivity.kt
@@ -2,6 +2,7 @@ package io.github.sunyufei.yyets
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var webView: WebView
+
+    private var backPressed: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,19 +39,32 @@ class MainActivity : AppCompatActivity() {
         webView.loadUrl(INDEX_URL)
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                if (url!!.indexOf("m.zimuzu.tv") > 0)
-                    webView.loadUrl(url)
-                else
+                if (webView.url == INDEX_URL && url!!.indexOf("m.zimuzu.tv") < 0)
                     Toast.makeText(this@MainActivity, "广告页面，不会跳转", Toast.LENGTH_SHORT).show()
+                else
+                    webView.loadUrl(url)
                 return true
             }
         }
+
+        webView.webChromeClient = object : WebChromeClient() {}
     }
 
     override fun onBackPressed() {
         when (webView.canGoBack()) {
-            true -> webView.goBack()
-            false -> super.onBackPressed()
+            true -> {
+                webView.goBack()
+                backPressed = false
+            }
+            false -> {
+                when (backPressed) {
+                    true -> super.onBackPressed()
+                    false -> {
+                        backPressed = true
+                        Toast.makeText(this@MainActivity, "再按一次退出", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
